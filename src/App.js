7f61a55960490df60b93f8097af4e437dec80a4e
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-03-31 18:32:47 -0500

    feat(App): Deficion de rutas de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import "./App.css";
-import ListaTareas from "./components/ListaTareas";
-import Logo from "./components/Logo";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Registro from "./components/Registro";
+import RequireAuth from "./utils/RequireAuth";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <main className="main-lista">
-        
-        <h1 className="titulo">Mis Tareas</h1>
-        <ListaTareas/>
+      <BrowserRouter>
+        <Routes>
+          {/* Ruta publica de registro, sin Header ni Footer */}
+          <Route path="/listas-tareas" element={<Registro />} />
 
-      </main>
+          {/* Rutas protegidas o privadas que utilizan el Layout con Header y Footer */} 
+          <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            
+            {/* Ruta para manejar páginas no encontradas 404 dentro del Layout */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
 
+          {/* Ruta catch-all para redirigir a Registro si se accede a una URL no válida */}
+          <Route path="*" element={<Navigate to="/listas-tareas" replace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
