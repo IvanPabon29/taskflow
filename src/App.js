31cae2728922aebb9260d35831f5e8a247d1ab8e
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-05-07 20:25:27 -0500

    feat(App): Agregar rutas necesarios para el funcionamiento de la app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Registro from "./components/Registro";
 import RequireAuth from "./utils/RequireAuth";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import CrearTablero from "./pages/CrearTablero";
+import Tablero from "./pages/Tablero";
+import MisTableros from "./pages/MisTableros";
+import MiPerfil from "./pages/MiPerfil";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
@@ -19,6 +23,11 @@ function App() {
           {/* Rutas protegidas o privadas que utilizan el Layout con Header y Footer */} 
           <Route path="/listas-tareas" element={<RequireAuth><Layout /></RequireAuth>}>
             <Route path="home" element={<Home />} />
+            <Route path="crear-tablero" element={<CrearTablero />} />
+            <Route path="tablero/:id" element={<Tablero />} />
+            
+            <Route path="mis-tableros" element={<MisTableros />} />
+            <Route path="mi-perfil" element={<MiPerfil />} />
             <Route path="acerca-de" element={<About />} />
             
             {/* Ruta para manejar páginas no encontradas 404 dentro del Layout */}
